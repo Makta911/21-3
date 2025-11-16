10d6597d26d642a1e6ecf65a884ea72157b4f109
matve <matvey.bosix@yandex.ru>
2025-11-16 22:11:52 +0800

    25 skypro

---
+++ blog/views.py
@@ -0,0 +1,49 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from .models import BlogPost
+
+
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        # Фильтрация опубликованных статей: выводим только с is_published=True
+        return BlogPost.objects.filter(is_published=True).order_by('-created_at')
+
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        # Увеличение счетчика просмотров при открытии статьи
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save()
+        return obj
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+    success_url = reverse_lazy('blog:post_list')
+
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+
+    def get_success_url(self):
+        # Перенаправление после редактирования на просмотр этой статьи
+        return reverse('blog:post_detail', kwargs={'pk': self.object.pk})
+
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
